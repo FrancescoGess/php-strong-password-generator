b1a243f568df16a575dfeef61e55f0d0ac824523
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-02-27 17:16:30 +0100

    creo condizione per far inserire all'utente un valore tra 4 e 30 caratteri

--- index.php
+++ index.php
@@ -9,6 +9,19 @@ function genPass ($length){
 }
     return $password;
 }
+
+if (isset($_GET['length'])){
+    //controllo
+    $lengthPassword = (int)$_GET['length'];
+    //condizione di caratteri tra 4 e 30
+    if ($lengthPassword >= 4 && $lengthPassword <= 30){
+        $passGenerata = genPass($lengthPassword);
+        echo '<h3> Password Generata: </h3>';
+        echo '<p>' . $passGenerata . '</p>';
+    } else {
+        echo '<p> La lunghezza della password deve essere compresa tra 4 e 30 caratteri. </p>';
+    }
+}
 ?>
 
 <!DOCTYPE html>
